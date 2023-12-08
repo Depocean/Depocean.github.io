16d49b8c4c00ef4080a427c2ef88ec816588bfa8
Depocean <3089446876@qq.com>
2023-12-08 17:59:15 +0800

    Site updated: 2023-12-08 17:59:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/02/cet/","2023/12/06/blog搭建/","2023/12/08/blog主题及美化/","2023/12/03/picture/"];function toRandomPost(){
+var posts=["2023/12/02/cet/","2023/12/08/blog主题及美化/","2023/12/03/picture/","2023/12/06/blog搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
